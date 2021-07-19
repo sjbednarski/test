a844f78cd96d1191eda71940ca1dc948dc66a499
sjbednarski <87493966+sjbednarski@users.noreply.github.com>
2021-07-18 18:01:29 -0700

    Add files via upload

---
+++ initialization.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+origin = Point(0,0)
+unit_circle = Circle(origin,1)
+
---
+++ methods.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import math
+
+def area(circle):
+    return math.pi*circle.r**2
+
