5166b2b1d76e083455135d9f9def60246395ad54
sjbednarski <87493966+sjbednarski@users.noreply.github.com>
2021-07-18 17:57:31 -0700

    Add files via upload

---
+++ objects.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Circle:
+    def __init__(self, center, radius):
+        self.center = center #a point
+        self.r = radius
+        
+
